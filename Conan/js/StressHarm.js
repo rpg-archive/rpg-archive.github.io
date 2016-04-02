bdfbe839d4913863574683dc82af3603db6d339a
rpg-archive <clab4950@alumni.uidaho.edu>
2016-04-01 23:16:05 -0400

    modified StressHarm.js to only color Stress and Harm cells

--- Conan/js/StressHarm.js
+++ Conan/js/StressHarm.js
@@ -1,10 +1,12 @@
 
 $(document).ready(function(){
     $("td").click(function(){
-		if ($(this).css('background-color') == 'rgb(225, 225, 214)') {
-   			$(this).css('background-color', 'rgb(46, 5, 5)');
-		}else{
-			$(this).css('background-color', 'rgb(225, 225, 214)');
-		}
+    	if( $(this).hasClass("StressHarm") ){
+			if ($(this).css('background-color') == 'rgb(225, 225, 214)') {
+	   			$(this).css('background-color', 'rgb(46, 5, 5)');
+			}else{
+				$(this).css('background-color', 'rgb(225, 225, 214)');
+			}
+		}	
     });
 });
