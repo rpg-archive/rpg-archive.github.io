c44d6c037f10795386d44bda335bfe35464684fa
rpg-archive <clab4950@alumni.uidaho.edu>
2016-04-08 16:57:33 -0400

    Fix Wounds heal color

--- Conan/js/StressHarm.js
+++ Conan/js/StressHarm.js
@@ -16,10 +16,18 @@ $(document).ready(function(){
 		// 	$(this).css('background-color', 'rgb(214, 214, 48)');
 		// }
   //   });
-    $("td").click(function(){
+
+
+
+
+
+
+    $("td").click(function(e){
     	if( $(this).hasClass("stressHarm") ){
 			if ($(this).css('background-color') == 'rgb(225, 225, 214)') {
 	   			$(this).css('background-color', 'rgb(46, 5, 5)');
+			}else if ( $(this).css('background-color') == 'rgb(46, 5, 5)' ) {
+				$(this).css('background-color', 'rgb(214, 214, 48)');
 			}else{
 				$(this).css('background-color', 'rgb(225, 225, 214)');
 			}
